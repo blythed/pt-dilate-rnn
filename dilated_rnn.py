f8ac523d47d2855d5ca780cb9bb47970b03a2251
Kashif Rasul <kashif.rasul@zalando.de>
2018-01-22 11:51:37 +0100

    fixed input stacking

--- dilated_rnn.py
+++ dilated_rnn.py
@@ -43,11 +43,14 @@ class DilatedRNN(nn.Module):
         # we do zero padding --> [x1, x2, x3, x4, x5, 0]
         # we want to have --> [[x1; x2], [x3; x4], [x_5; 0]]
         # where the length is dilated_num_steps
-        dilated_inputs = torch.cat((inputs[i * rate:(i + 1) * rate] for i in range(dilated_num_steps)), dim=0)
+        dilated_inputs = torch.stack([torch.cat(inputs[i * rate:(i + 1) * rate] , dim=0) for i in range(dilated_num_steps)])
+        # dilated_inputs is of size [dilated_num_steps, rate*batch_size, input_size]
 
-        # dilated_inputs is of size [rate*dilated_num_steps, batch_size, input_size]
         dilated_outputs, _ = cell(dilated_inputs)
-        # output is of size [rate*dilated_num_steps, batch_size, hidden_size]
+        # output is of size [dilated_num_steps, rate*batch_size, hidden_size]
+
+        # reshape it to [dilated_num_steps*rate, batch_size, hidden_size]
+        
 
         # remove padded zeros so output is [num_steps, batch_size, hidden_size]
         # and return
